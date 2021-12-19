5b632ec9b0771bcbf186fb83142ff0744798c68d
acid-override <x1@galaxia.us>
2021-12-19 17:05:18 -0500

    commit on master

--- JSExamples/Learning_Code/toggleCase.js
+++ JSExamples/Learning_Code/toggleCase.js
@@ -2,18 +2,20 @@ function toggleCase(string){
   //string is immutable therefore create new string
   var newString = "";
   for (let i = 0; i < string.length; i++){
-    if (string.charAt(i) === string.charAt(i).toUpperCase()){
-      newString += string.charAt(i).toLowerCase();
+    //if (string.charAt(i) === string.charAt(i).toUpperCase()){
+    if (string[i] === string[i].toUpperCase()){
+
+      newString += string[i].toLowerCase();
       //console.log('typeOf', string[i])
 
     }else {
-      newString += string.charAt(i).toUpperCase();
+      newString += string[i].toUpperCase();
     }
 
   }
   console.log(newString);
 }
 
-toggleCase('MaRio'); //'mArIO'
+toggleCase('MaRioOoOoOo'); //'mArIO'
 toggleCase('PRP'); //'prp'
 toggleCase('Good news, everyone!'); //'gOOD NEWS, EVERYONE!'
