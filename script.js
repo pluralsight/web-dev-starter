3e44b286ed2173ac6b56403abb5595120728189e
Acid-Override <x1@galaxia.us>
2021-12-23 08:44:56 -0500

    commit on master

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
-function titleCase(str) {
-    // set all letters in str to lower case
-    //split the strings into separate words
-    //iterate through array of words
-      //at each word
-        //iterage through the word
-          //convert first letter to upper case and concatentate with empty string
-           //concatentate rest of letters onto string
-    //return string
-  
-    var newString = ''
-    var arrayOfWords = []
-  //   var lowerCaseStr = ''
-  str = str.toLowerCase()
-    //console.log(str);
-  arrayOfWords = str.split(' ')
-  //console.log(arrayOfWords)
-  for (var i = 0; i < arrayOfWords.length; i++){
-    newString += arrayOfWords[i][0].toUpperCase()
-    for (var j = 1; j < arrayOfWords[i].length; j++){
-      newString += arrayOfWords[i][j]
-  
-      //console.log('newstring', newString)
-    }
-    newString += ' '
-    
-    
+
+var cardData = [
+  {
+    'issuer/network': 'Visa',  // card issuer (network)
+    prefixes: ['4'],	       // beginning digits
+    lengths: [13, 16, 19]      // lengths of card numbers
+  },
+  {
+    'issuer/network': 'Mastercard',
+    prefixes: ['51', '52', '53', '54', '55'],
+    lengths: [16]
+  },
+  {
+    'issuer/network': 'American Express',
+    prefixes: ['34', '37'],
+    lengths: [15]
+  },
+  {
+    'issuer/network': 'Diner\'s Club',
+    prefixes: ['38', '39'],
+    lengths: [14]
   }
-  newString = newString.trim();
-  return newString;
-  
-  }
-  
-  console.log(titleCase('peter piper')); //"Peter Piper"
-  console.log(titleCase('teXt mEssAge')); //"Text Message"
-  
+];
+
+function detectNetwork(cardNumber, cardData) {
+  // your solution here
+  console.log(cardData)
+}
+
+// example
+var network = detectNetwork('343456789012345', cardData);
+console.log(network); // --> 'American Express'  
   
