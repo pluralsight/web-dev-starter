55e865de6d955281a7d8f7b9d09265a01da47f76
acid-override <x1@galaxia.us>
2021-12-24 13:26:21 -0500

    commit on master

---
+++ JSExamples/HR_Lecture/isPrime.js
@@ -0,0 +1,13 @@
+// Time limit 45 minutes
+// Write a function isPrime that takes an integer (less than a billion) and returns true if the integer is prime and false if it is not
+// https://en.wikipedia.org/wiki/Prime_number
+
+
+
+isPrime(12);  //false
+isPrime(13);  //true
+isPrime(63);  //false
+isPrime(67);  //true
+isPrime(871); //false
+isPrime(15485863);  //true
+isPrime(15485869);  //false
