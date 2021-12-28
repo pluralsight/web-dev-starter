53077d0fa315e180e3bdc9a9056d248e099e78cf
Acid-Override <x1@galaxia.us>
2021-12-28 18:39:20 -0500

    commit on master

---
+++ script.js
@@ -0,0 +1,11 @@
+function repeatString(string, num) {
+    if(num === 0){
+        return '';
+    } else {
+        return string = string.concat(repeatString(string, num-1))
+  
+    }
+}
+
+var output = repeatString('code', 5);
+console.log(output); // --> 'codecodecode'
